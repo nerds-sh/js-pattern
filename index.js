016d144a4addad80eccf72f08eb39d31063716e7
miutamihai <miuta.mihai@gmail.com>
2021-05-19 15:04:36 +0300

    [master] Add like operator.

--- index.js
+++ index.js
@@ -12,10 +12,21 @@ const matchWith = (value, dict) => (condition, lambda) => {
     return match(value, dict)
 }
 
+const isLike = (value, comparison) => Object.keys(comparison)
+    .filter(key => value[key])
+    .every(key => value[key] === comparison[key])
+
+const like = (value, dict) => (comparison, lambda) => {
+    dict = {...dict, [isLike(value, comparison)]: matched => lambda(matched)}
+
+    return match(value, dict)
+}
+
 const otherwise = (value, dict) => lambda => dict[true] ? dict[true](value) : lambda(value)
 
 const match = (value, dict={}) => ({
     with: matchWith(value, dict),
+    like: like(value, dict),
     otherwise: otherwise(value, dict),
     execute: () => dict[true](value)
 })
