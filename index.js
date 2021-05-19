7f4e4449907ae988f2210f9a34a98f5585c72c10
miutamihai <miuta.mihai@gmail.com>
2021-05-19 14:05:48 +0300

    [master] Initial push.

---
+++ index.js
@@ -0,0 +1,20 @@
+const matchWith = (value, dict) => (condition, lambda) => {
+    if(typeof condition === 'function') {
+        dict = {...dict, [condition(value)]: matched => lambda(matched)}
+    }
+    else {
+        dict = {...dict, [condition]: matched => lambda(matched)}
+    }
+
+    return match(value, dict)
+}
+
+const otherwise = (value, dict) => lambda => dict[true] ? dict[true](value) : lambda(value)
+
+const match = (value, dict={}) => ({
+    with: matchWith(value, dict),
+    otherwise: otherwise(value, dict),
+    execute: () => dict[true](value)
+})
+
+module.exports = match
